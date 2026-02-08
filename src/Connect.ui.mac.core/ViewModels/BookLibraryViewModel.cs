6fa622fe6881c79bd21a7b7021eed67245d9b88c
David Obando <daobando@microsoft.com>
2026-02-08 08:51:56 -1000

    Add book details (#3)

--- src/Connect.ui.mac.core/ViewModels/BookLibraryViewModel.cs
+++ src/Connect.ui.mac.core/ViewModels/BookLibraryViewModel.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using core.audiamus.booksdb;
@@ -15,6 +17,13 @@ namespace core.audiamus.connect.ui.mac.ViewModels {
 
     [ObservableProperty]
     private BookItemViewModel _selectedBook;
+
+    [ObservableProperty]
+    private bool _hasSelectedBook;
+
+    partial void OnSelectedBookChanged (BookItemViewModel value) {
+      HasSelectedBook = value is not null;
+    }
 
     [ObservableProperty]
     private string _filterText;
@@ -42,6 +51,9 @@ namespace core.audiamus.connect.ui.mac.ViewModels {
         Books.Add (vm);
       }
       UpdateSelectedCount ();
+
+      if (Books.Count > 0)
+        SelectedBook = Books[0];
     }
 
     public IEnumerable<BookItemViewModel> GetSelectedBooks () =>
@@ -101,5 +113,49 @@ namespace core.audiamus.connect.ui.mac.ViewModels {
           : $"{ts.Minutes}m";
       }
     }
+
+    // Detail properties
+    public string Publisher => _book.PublisherName;
+    public string Language => _book.Language;
+    public string Unabridged => _book.Unabridged switch {
+      true => "Yes",
+      false => "No",
+      _ => null
+    };
+    public string Series => _book.Series?.Count > 0
+      ? string.Join (", ", _book.Series.Select (s => s.ToString ()))
+      : null;
+    public string ConversionStateText => ConversionState.ToString ();
+    public int? Parts => _book.Components?.Count > 0 ? _book.Components.Count : (int?)null;
+
+    public string Description {
+      get {
+        var html = _book.PublisherSummary;
+        if (string.IsNullOrWhiteSpace (html))
+          return null;
+        // Strip HTML tags and decode common entities
+        var text = Regex.Replace (html, "<[^>]+>", " ");
+        text = text.Replace ("&amp;", "&")
+                   .Replace ("&lt;", "<")
+                   .Replace ("&gt;", ">")
+                   .Replace ("&quot;", "\"")
+                   .Replace ("&#39;", "'")
+                   .Replace ("&nbsp;", " ");
+        // Collapse whitespace
+        text = Regex.Replace (text, @"\s+", " ").Trim ();
+        return text;
+      }
+    }
+
+    public string CoverImagePath {
+      get {
+        var path = _book.CoverImageFile;
+        if (!string.IsNullOrEmpty (path) && File.Exists (path))
+          return path;
+        return null;
+      }
+    }
+
+    public bool HasDetails => true;
   }
 }
