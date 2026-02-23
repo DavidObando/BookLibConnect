182a219741af0cdfa6d9846b41040d115060a21a
David Obando <daobando@microsoft.com>
2026-02-23 09:09:09 -0800

    Update homebrew formula to 1.0.27

--- Formula/oahu.rb
+++ Formula/oahu.rb
@@ -4,28 +4,28 @@
 class Oahu < Formula
   desc "Standalone Audible downloader and decrypter"
   homepage "https://github.com/DavidObando/Oahu"
-  version "1.0.25"
+  version "1.0.27"
   license "GPL-3.0-only"
 
   on_macos do
     on_arm do
       url "https://github.com/DavidObando/Oahu/releases/download/v#{version}/Oahu-#{version}-osx-arm64.tar.gz"
-      sha256 "bdbb7117d62dfcb7a58ca0a0610dbe576487097ce1e3f558c8c094b38fc213ca"
+      sha256 "2eeb1f048947663f508bc413dc0b40a575557d7b9a2e3f8e07d0bf3a4b8e06e0"
     end
     on_intel do
       url "https://github.com/DavidObando/Oahu/releases/download/v#{version}/Oahu-#{version}-osx-x64.tar.gz"
-      sha256 "1b45b6e2c0619d9a7b1a63e858f4d70844cf05dab8aac02c8d316347d44f0e0f"
+      sha256 "121994192789cab777029292fc8025414d0233d35da41d1aee57cbfc9d545353"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/DavidObando/Oahu/releases/download/v#{version}/Oahu-#{version}-linux-arm64.tar.gz"
-      sha256 "7e322d162ee6f1a241742e8065626cfb0eff801e777cb28bf2ae2dbea38f5bb0"
+      sha256 "c17839554305333eb3dea8f8a080273b9f3d4538596841033791713bfac97d13"
     end
     on_intel do
       url "https://github.com/DavidObando/Oahu/releases/download/v#{version}/Oahu-#{version}-linux-x64.tar.gz"
-      sha256 "f7b66dbc96f35da0afbac082e61be825c19f5fcfa2a614535fac6149c0fbddfb"
+      sha256 "6335a2f43b05d2f5736d55ba6abfa555cf538cc4ae98ef5929f70eaea7bb9701"
     end
   end
 
