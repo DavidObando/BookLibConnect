b7f98529ddf57854a97b44e912d2b29621fbfd60
David Obando <daobando@microsoft.com>
2026-03-29 16:29:28 -0700

    Add button to open downloads folder

--- src/Oahu.Foundation/Auxiliary/ShellExecute.cs
+++ src/Oahu.Foundation/Auxiliary/ShellExecute.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Diagnostics;
+using System.IO;
 
 namespace Oahu.Aux
 {
@@ -15,6 +16,54 @@ namespace Oahu.Aux
         CreateNoWindow = true,
         FileName = url,
       });
+    }
+
+    public static void Directory(string path)
+    {
+      if (string.IsNullOrWhiteSpace(path))
+      {
+        throw new ArgumentException("Directory path is required.", nameof(path));
+      }
+
+      System.IO.Directory.CreateDirectory(path);
+
+      ProcessStartInfo startInfo;
+      if (OperatingSystem.IsWindows())
+      {
+        startInfo = new ProcessStartInfo
+        {
+          FileName = "explorer.exe",
+          ArgumentList = { path },
+          UseShellExecute = false,
+          CreateNoWindow = true,
+        };
+      }
+      else if (OperatingSystem.IsMacOS())
+      {
+        startInfo = new ProcessStartInfo
+        {
+          FileName = "open",
+          ArgumentList = { path },
+          UseShellExecute = false,
+          CreateNoWindow = true,
+        };
+      }
+      else if (OperatingSystem.IsLinux())
+      {
+        startInfo = new ProcessStartInfo
+        {
+          FileName = "xdg-open",
+          ArgumentList = { path },
+          UseShellExecute = false,
+          CreateNoWindow = true,
+        };
+      }
+      else
+      {
+        throw new PlatformNotSupportedException("Opening directories is not supported on this platform.");
+      }
+
+      Process.Start(startInfo);
     }
   }
 }
